b7c5b62fb5b362275cffb19efc38b53324ceb7a4
AlexNesvit <alex.nesvit2000@gmail.com>
2024-06-04 17:11:01 +0200

    modif V1:10

---
+++ assets/showAndHiddePassword.js
@@ -0,0 +1,14 @@
+const eyeOn = document.querySelector(".eye-on");
+const eyeOff = document.querySelector(".eye-off");
+const inputPassword = document.querySelector("#inputPassword");
+
+eyeOn.addEventListener("click", () => {
+    eyeOn.style.display = "none";
+    eyeOff.style.display = "block";
+    inputPassword.type = "text";
+});
+eyeOff.addEventListener("click", () => {
+    eyeOff.style.display = "none";
+    eyeOn.style.display = "block";
+    inputPassword.type = "password";
+});
